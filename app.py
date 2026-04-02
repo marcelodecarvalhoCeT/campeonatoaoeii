4182f1ed1e0054dade69f6cf7a4161b61b374505
marcelodecarvalhoCeT <marcelo.mello.007@ufrn.edu.br>
2026-04-02 00:44:06 -0300

    erro @def salvar_no_sheets():

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ with st.sidebar:
     # O botão agora chama a sua função correta
     if st.button("💾 Salvar Alterações na Nuvem", use_container_width=True):
         with st.spinner("Enviando para o Google Sheets..."):
-            salvar_no_sheets()
+           def salvar_no_sheets()
             st.success("Tudo salvo com sucesso!")
 
 # 4. INICIALIZAÇÃO DO ESTADO
